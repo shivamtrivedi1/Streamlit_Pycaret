f6eef9b4925c35fc35bc67c83aab33561afa22b5
shivamtrivedi1 <40827877+shivamtrivedi1@users.noreply.github.com>
2022-03-21 16:29:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ def run():
     if add_selectbox == 'Online':
 
         Age = st.number_input('Age', min_value=1, max_value=100, value=25)
-        Sex = st.selectbox('Sex', ['male', 'female'])
+        Sex = st.selectbox('Sex', ['male', 'female'],value='female')
         Pclass= st.number_input('P Class', 1,3)
-        SibSp=  st.multiselect('Number of Siblings And Spouse',[0,1,2,3,4,5,8])
-        Parch= st.multiselect('Parch',[0,1,2,3,4,5,6])
+        SibSp=  st.selectbox('Number of Siblings And Spouse',[0,1,2,3,4,5,8],value=0)
+        Parch= st.selectbox('Parch',[0,1,2,3,4,5,6],value=0)
         Fare=  st.slider('Fare', 0,600)
-        Embarked = st.selectbox('Embarked', ['S', 'C', 'Q'])
+        Embarked = st.selectbox('Embarked', ['S', 'C', 'Q'],value='S')
     
         output=""
 
